ccaa6723199cc92d83723142ed37519144878402
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-09 21:36:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand, :unique_brand
   
   BRANDS = ["Uggs","Rainbow"]
 
@@ -18,6 +18,8 @@ class Shoe
   end
   
   def unique_brand(unique_brand)
-    @unique_brand =
+    @unique_brand = unique_brand
+    BRANDS << unique_brand
+  end
 
 end
